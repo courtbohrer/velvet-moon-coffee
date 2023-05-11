544bbb71a7b3bb3e8d6ef84051d56aa4b0f1b9f5
Courtney Bohrer <cbohrer@utexas.edu>
2023-05-10 20:45:05 -0400

    firebase init

--- src/App.js
+++ src/App.js
@@ -8,6 +8,29 @@ import '@fontsource/montserrat';
 import '@fontsource/bowlby-one';
 import "@fontsource/titan-one"
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeoCHtagZmw3z1XVWpUyab_4vmDJZ44RY",
+  authDomain: "velvet-moon-coffee.firebaseapp.com",
+  projectId: "velvet-moon-coffee",
+  storageBucket: "velvet-moon-coffee.appspot.com",
+  messagingSenderId: "1021297187774",
+  appId: "1:1021297187774:web:c45ef9af573aa8e147f7ef",
+  measurementId: "G-WGXN7CS7C4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const theme = extendTheme({
   fonts: {
     heading: `"Titan One", cursive`,
